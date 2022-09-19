3ffa175a5e3a747db575eca611cda7e9345034c7
Martoxdlol <martoxdlol@gmail.com>
2022-09-19 14:04:45 -0300

    cd and [ ]

--- frontend/build_and_run.sh
+++ frontend/build_and_run.sh
@@ -1,11 +1,10 @@
 FILE=.next
-if [[ -d "$FILE" ]]; then
+if [ -d "$FILE" ]; then
     echo "$FILE exists."
 else
     echo "$FILE don't exist."
     sleep 30
     npm run build
 fi
-cd frontend
 npm run next_start
 
